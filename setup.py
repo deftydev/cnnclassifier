6d9370cf82392b7e2d35b62d42b1cd7dde623809
deftydev <devanshgupta79212346@gmail.com>
2023-09-29 19:30:15 +0530

    Project template updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+__version__="0.0.1"
+
+REPO_NAME="cnnclassifier"
+AUTHOR_NAME="deftdev"
+SRC_REPO="cnnclassifier"
+AUTHOR_EMAIL="devanshgupta79212346@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME
+    
+)
